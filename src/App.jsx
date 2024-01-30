1778a311901d1bf9e2ebe8898b6dac79690a7d24
Asmita <102907009+AsmitaOjha@users.noreply.github.com>
2024-01-30 06:21:03 +0000

    Modified portfolio according to my details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Asmita Ojha",
+  title: "Computer Science Student & Web Developer",
+  email: "asmitaojha05@gmail.com.",
+  gitHub: "https://github.com/AsmitaOjha",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/asmita-ojha-aa313a236",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/laptop-in-table.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "laptop in table";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Pani Puri Website template🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I create a pani puri website template in a learning journey of HTML & CSS. I choosed pani puri as my content because of it's popurality and uniqueness.",
+    url: "https://asmitaojha.github.io/PaniPuri_website/index.html",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Clone-of-Spotify🎶",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Taking reference from youtube I tried to make a clone of spotify using HTML,CSS & Javascript",
+    url: "https://github.com/AsmitaOjha/Clone-of-Spotify",
   },
   {
-    title: "My Resume Site",
+    title: "Combine Project 'TeenSathi🎓' ",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created a Teensathi website template with my friend Rohit. It is based on the idea to provide all information a teenage needs about their mental and physical health.",
+    url: "https://github.com/AsmitaOjha/Combined_Project_TeenSathi",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Bank System💵",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I tried to make a bank system using HTML,CSS and php. The idea of making this project was given by our Teacher. This system has facility to create account, deposit, withdraw and transer.",
+    url: "https://github.com/AsmitaOjha/SAD_project_Bank_system",
   },
 ];
 
