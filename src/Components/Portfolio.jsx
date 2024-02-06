ffee7eceac207d0c7950cf540411e9a0ca959dfa
Asmita <102907009+AsmitaOjha@users.noreply.github.com>
2024-02-06 06:31:10 +0000

    deleted one

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/laptop-in-table.jpg";
+import image from "../images/me reading book.jpg";
 
 const imageAltText = "laptop in table";
 
