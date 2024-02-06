7bb531bd075e95f3784cbe649f275aa4fa533a46
Asmita <102907009+AsmitaOjha@users.noreply.github.com>
2024-02-06 08:03:59 +0000

    I am done

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/rose in my book.jpg";
+import image from "../images/design-desk.jpeg";
 
-const imageAltText = "rose in my book";
+const imageAltText = "desk for working";
 
 /**
  * Project list
