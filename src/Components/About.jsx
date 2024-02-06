c3b501ebae1b782a3027b24a0b393f50ab922801
Asmita <102907009+AsmitaOjha@users.noreply.github.com>
2024-02-06 07:10:55 +0000

    resolving ' git commit -m resolving exit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/top-section.jpg";
+import image from "../images/book in my hand.jpg";
 
 const imageAltText = "background of a natural place.";
 
